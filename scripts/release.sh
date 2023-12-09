46d5d2ef50a6c1e67041e852248807321f98c462
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2023-12-09 09:20:55 -0800

    fix release script so it does not print

--- scripts/release.sh
+++ scripts/release.sh
@@ -96,7 +96,5 @@ git push --tags $REMOTE
 if [[ -e "${GOLLYX_DRAGON_PELICAN_HOME}/environment.${PROMOTE_DEST_BRANCH}" ]]; then
     source "${GOLLYX_DRAGON_PELICAN_HOME}/environment.${PROMOTE_DEST_BRANCH}"
     make -C "$GOLLYX_DRAGON_PELICAN_HOME" deploy
-else
-    echo "Could not find environment config file ${GOLLYX_DRAGON_PELICAN_HOME}/environment.${PROMOTE_DEST_BRANCH}. Unable to deploy."
 fi
 
